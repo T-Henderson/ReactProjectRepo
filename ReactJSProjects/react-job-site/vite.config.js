856e00b1f08ff92330d7ef16b264b113b1b7530a
T-Henderson <ethenderson15@gmail.com>
2025-05-06 13:44:45 -0400

    Second Commit

--- ReactJSProjects/react-job-site/vite.config.js
+++ ReactJSProjects/react-job-site/vite.config.js
@@ -8,5 +8,12 @@ export default defineConfig({
   "@tailwindcss/postcss": {},
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   },
 })
